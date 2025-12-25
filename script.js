0a17989a4b038d5466f40737f4a3ee28c2c10be6
ali-hmed <aliahmedxdev9@gmail.com>
2025-12-25 22:36:31 +0300

    newClock

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+function updateClock(){
+    const now=new Date();
+    const hours=String(now.getHours()).padStart(2, '0');
+    const minutes=String(now.getMinutes()).padStart(2, '0');
+    const seconds=String(now.getSeconds()).padStart(2, '0');
+    const ampm=hours>=12?'PM':'AM';
+
+    document.querySelector('.hours-min-sec').textContent=`${hours}:${minutes}:${seconds} ${ampm}`;
+
+    // document.querySelector('.am-pm').textContent= ampm;
+
+//   document.querySelector('.date').textContent=`${getDay()} ${getMonth()} ${getFullYear()}`;
+
+const option={
+    weekday:'long',
+    year:'numeric',
+    month:'long',
+    day:'numeric'
+}
+document.querySelector('.date').textContent=now.toLocaleDateString(undefined, option);
+
+
+
+}
+// updateClock()
+setInterval(updateClock, 1000);
